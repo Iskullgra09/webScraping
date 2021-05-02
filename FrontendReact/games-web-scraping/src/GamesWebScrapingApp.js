3111c00f5937fac372920a3d62e058ffdf0562d7
carlosvillalobossalas <carlosvillalobos247@gmail.com>
2021-05-02 17:06:34 -0600

    pagination

--- FrontendReact/games-web-scraping/src/GamesWebScrapingApp.js
+++ FrontendReact/games-web-scraping/src/GamesWebScrapingApp.js
@@ -1,24 +1,77 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import ReactPaginate from 'react-paginate';
 import Game from './components/Game'
 import './styles.css'
 
 const GamesWebScrapingApp = () => {
 
-  const [games, setGames] = useState(['1', '2', '3', '4', '5', '6', '7', '8', '9']);
+  // const [games, setGames] = useState(['1', '2', '3', '4', '5', '6', '7', '8', '9']);
+
+  //Pagination
+  const [photos, setPhotos] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [photosPerPage] = useState(20);
 
   useEffect(() => {
+    //fetch example
+    const fetchPhotos = async () => {
+      setLoading(true);
+
+      const res = await axios.get('https://jsonplaceholder.typicode.com/photos');
+      setPhotos(res.data);
+
+      setLoading(false);
+
+    }
+
+    fetchPhotos();
     //fetch real data
   }, []);
 
 
+  //Get current photos
+  const indexOfLastPhoto = currentPage * photosPerPage;
+  const indexOfFirstPhoto = indexOfLastPhoto - photosPerPage;
+  const currentPhotos = photos.slice(indexOfFirstPhoto, indexOfLastPhoto);
+
+  //change paginate
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber.selected + 1)
+  };
+
+  //Pagination npm
+  const numberOfPages = Math.ceil((photos.length) / photosPerPage);
+
   return (
-    <div className="game-container">
-      {
-        games.map((game, index) => (
-          <Game key={index} />
-        ))
-      }
-    </div>
+    <>
+      <header>
+        <div className="commentBox">
+          <ReactPaginate
+            initialPage={1}
+            previousLabel="<"
+            nextLabel=">"
+            breakClassName={'break-me'}
+            pageCount={numberOfPages}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={3}
+            onPageChange={paginate}
+            containerClassName={'pagination'}
+            activeClassName={'active'}
+            nextLinkClassName={'pagination-next'}
+            previousLinkClassName={'pagination-prev'}
+          />
+        </div>
+      </header>
+      <div className="game-container">
+        {
+          currentPhotos.map((game, index) => (
+            <Game key={index} data={game} />
+          ))
+        }
+      </div>
+    </>
   )
 }
 
--- FrontendReact/games-web-scraping/src/components/Game.js
+++ FrontendReact/games-web-scraping/src/components/Game.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import srcImg from '../assets/modern-warfare.jpg'
 
-const Game = () => (
-    <div className="game">
-        <img src={srcImg} alt="COD" />
-        <div className="game-info">
-            <h5>Call of Duty: Modern Warfare</h5>
-            <span>Precio: 60$</span>
-            <span>Puntuacion: 9.5</span>
-            <span>Duracion: 19h</span>
+const Game = ({ data }) => {
+
+    console.log('Data', data)
+    return (
+        <div className="game">
+            <img src={data.url} alt="COD" />
+            <div className="game-info">
+                <h3>Call of Duty: Modern Warfare</h3>
+                <span>Precio: 60$</span>
+                <span>Puntuación: {data.id}</span>
+                <span>Duración: 19h</span>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default Game
