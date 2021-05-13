cc509ac4d86bb7f6523d7905f9a00bef7024477e
Isaac José Granados Quesada <isaac_granados09@hotmail.com>
2021-05-12 20:14:42 -0600

    -Etiqueta de oferta

--- FrontendReact/games-web-scraping/src/components/Game.js
+++ FrontendReact/games-web-scraping/src/components/Game.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import './Game.css'
 
 const Game = ({ data }) => {
 
     return (
         <div className="game">
+            {
+                data.offer ? (
+                    <div className="ajuste">
+                    <span className="popular-badge">
+                    <span>OFERTA</span>
+                    </span>
+                    </div>
+                ) : null
+            }
             <img src={data.imageURL} alt="COD" />
             <div className="game-info">
-                <h3>{data.name}</h3>
+                <div className="name">
+                    <p>{data.name}</p>
+                </div>
                 <span>{data.price}</span>
                 <span>Puntuación: {data.qualification}</span>
                 <span>Duración: {data.hltb}</span>
